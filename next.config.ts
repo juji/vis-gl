ce3bb8b06821a2e68f5d69397a150277b7ec1dc2
juji <jujiyangasli@gmail.com>
2025-09-05 17:51:04 +0700

    add serwist

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+import withSerwistInit from "@serwist/next";
+
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist(nextConfig);
