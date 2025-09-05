add730bd84b3e6da17af01adcd4bb780b77710a3
juji <jujiyangasli@gmail.com>
2025-09-05 19:19:24 +0700

    test list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <div>test changed</div>
       </body>
     </html>
   );
