bd42d90137ef325d9a0825a906c840357b856ca2
juji <jujiyangasli@gmail.com>
2025-10-02 04:54:57 +0700

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
-import 'sanitize.css';
-import 'sanitize.css/forms.css';
-import 'sanitize.css/typography.css';
-import 'sanitize.css/assets.css';
+import "sanitize.css";
+import "sanitize.css/forms.css";
+import "sanitize.css/typography.css";
+import "sanitize.css/assets.css";
 import "./globals.css";
-import 'sanitize.css/reduce-motion.css';
+import "sanitize.css/reduce-motion.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,8 +19,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Juji | Web Developer",
-  description: "Welcome to my portfolio website."
+  title: "vis.gl example",
+  description: "Example of drawing google maps.",
 };
 
 export default function RootLayout({
@@ -32,7 +32,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
-        <div>test changed</div>
       </body>
     </html>
   );
